fb4894d6c194e6ce2308793c5758fbfdf9577d28
DarkEmBasSy666 <67989743+jganz6@users.noreply.github.com>
2021-03-30 09:42:50 +0700

    add fix

--- src/index.js
+++ src/index.js
@@ -4,15 +4,15 @@ import Login from "./pages/Login";
 import Main from "./pages/Main";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-// import ForgotPassword from "./pages/ForgotPassword";
-import GetOTP from "./components/getOTP";
+import ForgotPassword from "./pages/ForgotPassword";
+// import GetOTP from "./components/getOTP";
 import Register from "./pages/Register";
 const AppWithRouter = () => {
   return (
     <Router>
       <Route path="/" exact component={Login} />
       <Route path="/Main/:content?/:content2?" component={Main} />
-      <Route path="/ForgotPassword" component={GetOTP} />
+      <Route path="/ForgotPassword" component={ForgotPassword} />
       <Route path="/Register" component={Register} />
     </Router>
   );
